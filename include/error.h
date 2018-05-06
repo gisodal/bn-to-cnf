0a48176a5a86cfa704270aafde707a8d36a64252
gdal@thinkpad <gdal@cs.ru.nl>
2018-05-07 01:36:32 +0200

    c style throw

--- include/error.h
+++ include/error.h
@@ -1,23 +1,23 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-#include <exception/exception.h>
+#include <stdarg.h>
+#include <stdio.h>
 
-typedef string_exception throw_string_error;
+#define THROW_BUFFER_SIZE 512
+class throw_string_error : public std::string, public std::exception {
+    private:
+        char buffer[THROW_BUFFER_SIZE];
+    public:
+        throw_string_error( const char* format, ... ) {
+            va_list args;
+            va_start(args, format);
+            vsprintf(buffer,format, args);
+            va_end(args);
 
-//#define THROW_BUFFER_SIZE 512
-//class throw_string_error {
-//    char err[THROW_BUFFER_SIZE];
-//    public:
-//        throw_string_error(const char *format, ...){
-//            va_list args;
-//            va_start (args, format);
-//            vsnprintf(err, THROW_BUFFER_SIZE-1, format, args);
-//            va_end(args);
-//            err[THROW_BUFFER_SIZE-1] = '\0';
-//        };
-//        char* what() { return err; };
-//
-//};
+        };
+        ~throw_string_error() throw() {};
+        const char* what() const throw() { return buffer; } ;
+};
 
 #endif
